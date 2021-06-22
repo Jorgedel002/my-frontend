119126cfabaf0319ce5868eb33fd5bbb7a72a1e0
Jorgedel002 <71532870+Jorgedel002@users.noreply.github.com>
2021-06-22 16:12:09 -0300

    22/6

--- portfolio-2021/index.js
+++ portfolio-2021/index.js
@@ -1,6 +1,17 @@
 const iconNav = document.querySelector('#hamburger'),
+       open = document.querySelector('#open'),
+       close = document.querySelector('#close'),
       navList = document.querySelector('#nav-bar');
+      windowC = document.querySelector('#window');
 
       iconNav.addEventListener('click', () =>{
           navList.classList.toggle('active');
       })
+
+       open.addEventListener('click', (e) => {
+       windowC.classList.toggle('active-w');
+      })
+
+       close.addEventListener('click', (e) => {
+       windowC.classList.toggle('active-w');
+     })
